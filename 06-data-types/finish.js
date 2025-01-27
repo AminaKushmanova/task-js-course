15165447e5e0f99feaeec0311ca5db6166d95fd8
AminaKushmanova <amina.kushmanova@alg-in.kz>
2025-01-27 22:23:13 +0500

    ЗАДАЧА 6 - Типы данных

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,18 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+const stringElem = 'привет'
+const numberElem = 54
+const booleanElem = true
+const nullElem = null
+const undefinedElem = undefined
+const objectElem = { a: 1, b: 2, c: 3, d: 4 }
+const arrayElem = [1, 2, 3, 4]
+
+console.log(typeof stringElem)
+console.log(typeof numberElem)
+console.log(typeof booleanElem)
+console.log(typeof nullElem)
+console.log(typeof undefinedElem)
+console.log(typeof objectElem)
+console.log(typeof arrayElem)
